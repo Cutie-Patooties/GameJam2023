c12f083c60810738b4c3d270b3ef6eb1a0f55258
SanicSpeed1238 <guock1238wa@gmail.com>
2023-09-29 03:14:05 -0500

    added sprite animations

--- game/GameJamProj/Assets/Scripts/Player/PlayerController.cs
+++ game/GameJamProj/Assets/Scripts/Player/PlayerController.cs
@@ -16,11 +16,14 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 1f;
     public Vector2 lastMovementDirection;
     private Rigidbody2D playerRigidBody;
+    private Animator playerAnimation;
+    private string currentTrigger = "";
 
     // Receive and Set all Necessary Components
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     private void Update()
@@ -34,7 +37,36 @@ public class PlayerController : MonoBehaviour
         playerRigidBody.velocity = movementSpeed * movementInput;
 
         if (movementInput != Vector2.zero)
+        {
             lastMovementDirection = movementInput.normalized;
+
+            if (Mathf.Abs(lastMovementDirection.x) > Mathf.Abs(lastMovementDirection.y))
+            {
+                if (lastMovementDirection.x > 0 && currentTrigger != "MoveRight")
+                {
+                    playerAnimation.SetTrigger("MoveRight");
+                    currentTrigger = "MoveRight";
+                }
+                else if (lastMovementDirection.x < 0 && currentTrigger != "MoveLeft")
+                {
+                    playerAnimation.SetTrigger("MoveLeft");
+                    currentTrigger = "MoveLeft";
+                }
+            }
+            else
+            {
+                if (lastMovementDirection.y > 0 && currentTrigger != "MoveUp")
+                {
+                    playerAnimation.SetTrigger("MoveUp");
+                    currentTrigger = "MoveUp";
+                }
+                else if (lastMovementDirection.y < 0 && currentTrigger != "MoveDown")
+                {
+                    playerAnimation.SetTrigger("MoveDown");
+                    currentTrigger = "MoveDown";
+                }
+            }
+        }
     }
 
     private void OnDisable()
